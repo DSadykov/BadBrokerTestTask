98002c8879f869ab98e3959b9c1b0c63cc20f3ce
Садыков Денис <d.sadykov@inbox.ru>
2022-11-13 21:51:50 +0500

    updated test for ExchangeRatesLoader

--- BadBrokerTestTask.Tests/Services/ExchangeRatesLoaderTests.cs
+++ BadBrokerTestTask.Tests/Services/ExchangeRatesLoaderTests.cs
@@ -22,28 +22,24 @@ namespace BadBrokerTestTask.Services.Tests
         public async void GetCurrencyRatesTest_ShouldBeOk()
         {
             //Arrange
-
-            var appSettings = @"{
-  ""ApiKey"": ""d35f20fe42aa46c28db5bb0b5df7c1bf"",
-  ""RequiredCurrencies"": ""rub,eur,gbp,jpy""
-}
-";
-
-            var builder = new ConfigurationBuilder();
-
-            builder.AddJsonStream(new MemoryStream(Encoding.UTF8.GetBytes(appSettings)));
-
-            var configuration = builder.Build();
             var logger1 = new Mock<ILogger<OpenExchangeRatesClient>>();
-            OpenExchangeRatesClient openExchangeRatesClient = new(configuration, logger1.Object);
-
             DateTime date = new(2022, 11, 13);
+            var openExchangeRatesClient = new Mock<IOpenExchangeRatesClient>();
+            openExchangeRatesClient.Setup(x => x.GetRatesForADate(date)).ReturnsAsync(new CurrencyRateModel()
+            {
+                DateTime = date,
+                Rates = new Dictionary<string, decimal>()
+            {
+                {"eur",(decimal)0.95 },
+                {"rub",60 }
+            }
+            });
 
-            var logger2 = new Mock<ILogger<ExchangeRatesLoader>>().Object;
 
-            var exchangeRatesLoader = new ExchangeRatesLoader(logger2, openExchangeRatesClient);
 
-            var _requierdCurrencies = configuration["RequiredCurrencies"].Split(',').Select(x => x.ToLower()).ToList();
+            var logger = new Mock<ILogger<ExchangeRatesLoader>>().Object;
+
+            var exchangeRatesLoader = new ExchangeRatesLoader(logger, openExchangeRatesClient.Object);
 
             //Act
 
@@ -53,8 +49,7 @@ namespace BadBrokerTestTask.Services.Tests
 
             Assert.NotNull(result);
             Assert.True(result.Count == 1);
-            Assert.True(result[0].Rates.Count == 4);
-            Assert.All(result[0].Rates.Keys, x => _requierdCurrencies.Contains(x));
+            Assert.True(result[0].Rates.Count == 2);
             Assert.True(result[0].DateTime == date);
         }
     }
