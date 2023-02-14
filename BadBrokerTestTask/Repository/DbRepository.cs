c8c95e2dee6335b5e414aec44a6ce31d75541d17
Садыков Денис <d.sadykov@inbox.ru>
2023-02-14 22:52:53 +0500

    update dbrepository to work with context

--- BadBrokerTestTask/Repository/DbRepository.cs
+++ BadBrokerTestTask/Repository/DbRepository.cs
@@ -9,9 +9,11 @@ using System.Xml.Linq;
 
 using BadBrokerTestTask.Interfaces;
 using BadBrokerTestTask.Models;
+using BadBrokerTestTask.Models.Responses;
 
 using Dapper;
 
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
@@ -19,79 +21,19 @@ namespace BadBrokerTestTask.Repository
 {
     public class DbRepository : IDbRepository
     {
-        private readonly SqlConnectionStringBuilder _connectionBuilder;
+        private readonly RatesDbContext _ratesDbContext;
         private readonly ILogger<DbRepository> _logger;
 
-        public DbRepository(IConfiguration configuration, ILogger<DbRepository> logger)
+        public DbRepository(RatesDbContext ratesDbContext, ILogger<DbRepository> logger)
         {
-            _connectionBuilder = new SqlConnectionStringBuilder(configuration.GetConnectionString("Default"));
+            _ratesDbContext = ratesDbContext;
             _logger = logger;
-            EnsureCreatedDb();
-            EnsureCreatedTable();
-
-        }
-        private void EnsureCreatedTable()
-        {
-            
-            using IDbConnection db = new SqlConnection(_connectionBuilder.ConnectionString);
-            try
-            {
-                var sqlQuery = "SELECT TOP(1) * FROM Rates";
-                db.Execute(sqlQuery);
-            }
-            catch (SqlException)
-            {
-                var sqlQuery = @"CREATE TABLE Rates( 
-                    Id int identity(1,1) PRIMARY KEY, 
-                    Currency nvarchar(20) , 
-                    Date datetime not null , 
-                    Rate decimal(10,6) NOT NULL 
-                    )";
-                db.Execute(sqlQuery);
-                _logger.LogInformation("Successfully created table Rates");
-            }
-        }
-
-        private void EnsureCreatedDb()
-        {
-            var dbName = _connectionBuilder.InitialCatalog;
-            _connectionBuilder.InitialCatalog = "";
-            using IDbConnection db = new SqlConnection(_connectionBuilder.ConnectionString);
-            try
-            {
-                db.Open();
-                db.ChangeDatabase(dbName);
-            }
-            catch (SqlException x)
-            {
-                if (x.Message.StartsWith($"Database '{dbName}' does not exist"))
-                {
-                    var sqlQuery = $"CREATE DATABASE {dbName}";
-                    db.Execute(sqlQuery);
-                    _logger.LogInformation($"Successfully created database {dbName}");
-                }
-                else
-                {
-                    _logger.LogError("Error while connecting to DB");
-                    throw;
-                }
-            }
-            _connectionBuilder.InitialCatalog = dbName;
         }
 
         public async Task<List<CurrencyRateModel>> GetRatesAsync(DateTime from, DateTime to)
         {
-            using IDbConnection db = new SqlConnection(_connectionBuilder.ConnectionString);
-            var sqlQuery = @"select [Currency]
-      ,[Date]
-      ,[Rate]
-	  from [Rates]
-	  where date between @from and @to
-      order by date";
-
-            IEnumerable<CurrencyRateDbModel> queryResult = await db.QueryAsync<CurrencyRateDbModel>(sqlQuery, new { from, to });
             List<CurrencyRateModel> result = new();
-            foreach (IGrouping<DateTime, CurrencyRateDbModel> groupedCurrencyRateDbModel in queryResult.GroupBy(x => x.Date))
+            foreach (IGrouping<DateTime, CurrencyRateDbModel> groupedCurrencyRateDbModel in _ratesDbContext.CurrencyRateModels.GroupBy(x => x.Date))
             {
                 CurrencyRateModel currencyRateModel = new()
                 {
@@ -111,24 +53,18 @@ namespace BadBrokerTestTask.Repository
 
         public async Task AddRatesAsync(List<CurrencyRateModel> currencyRateModels)
         {
-            using IDbConnection db = new SqlConnection(_connectionBuilder.ConnectionString);
-            var sqlQuery = @"if((select count(*) from Rates where Date=@date and @currency=Currency)=1)
-	return
-else
-	INSERT INTO [dbo].[Rates]
-	           ([Currency]
-	           ,[Date]
-	           ,[rate])
-	     VALUES
-	           (@currency
-	           ,@date
-	           ,@rate)";
             foreach (CurrencyRateModel currencyRateModel in currencyRateModels)
             {
-                foreach (KeyValuePair<string, decimal> rate in currencyRateModel.Rates)
+                IEnumerable<CurrencyRateDbModel> currencyRateDbModels = currencyRateModel.Rates.Select(rate => new CurrencyRateDbModel()
                 {
-                    await db.ExecuteAsync(sqlQuery, new CurrencyRateDbModel() { Currency = rate.Key, Date = currencyRateModel.DateTime, Rate = rate.Value });
-                    _logger.LogInformation($"Successfully added rate {currencyRateModel.DateTime} - {rate.Key} - {rate.Value} to database");
+                    Currency = rate.Key,
+                    Date = currencyRateModel.DateTime,
+                    Rate = rate.Value
+                });
+                await _ratesDbContext.AddRateModels(currencyRateDbModels);
+                foreach (var rate in currencyRateDbModels)
+                {
+                    _logger.LogInformation($"Successfully added rate {currencyRateModel.DateTime} - {rate.Currency} - {rate.Rate} to database");
                 }
             }
         }
