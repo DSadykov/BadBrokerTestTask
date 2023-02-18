7832f88e9a6ddf33c0b54ebfa08b510036886efc
Садыков Денис <d.sadykov@inbox.ru>
2023-02-18 10:59:02 +0500

    fix table name

--- BadBrokerTestTask/Models/CurrencyRateDbModel.cs
+++ BadBrokerTestTask/Models/CurrencyRateDbModel.cs
@@ -1,9 +1,14 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace BadBrokerTestTask.Models
 {
+    [Table("Rates")]
     public class CurrencyRateDbModel
     {
+        [Key]
+        public int Id { get; set; }
         public string Currency { get; set; }
         public DateTime Date { get; set; }
         public decimal Rate { get; set; }
